0354b4e27ed76f1e6bc39443ee118d41c44f2c07
Marvin Leiva <marleiq@hotmail.com>
2023-04-30 23:16:32 -0600

    Categories

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
-
+use App\Http\Controllers\Backend\EmployeeController;
+use App\Http\Controllers\Backend\CustomerController;
+use App\Http\Controllers\Backend\SupplierController;
+use App\Http\Controllers\Backend\SalaryController;
+use App\Http\Controllers\Backend\AttendenceController;
+use App\Http\Controllers\Backend\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -44,4 +49,14 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/change/password', [AdminController::class, 'ChangePassword'])->name('change.password');
 
     Route::post('/update/password', [AdminController::class, 'UpdatePassword'])->name('update.password');
+
+    ///Categorias All Route 
+    Route::controller(CategoryController::class)->group(function () {
+
+        Route::get('/all/category', 'AllCategory')->name('all.category');
+        Route::post('/store/category', 'StoreCategory')->name('category.store');
+        Route::get('/edit/category/{id}', 'EditCategory')->name('edit.category');
+        Route::post('/update/category', 'UpdateCategory')->name('category.update');
+        Route::get('/delete/category/{id}', 'DeleteCategory')->name('delete.category');
+    });
 }); // End User Middleware 
